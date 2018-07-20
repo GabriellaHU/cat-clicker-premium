cf80e83ccc77d0dca9c79570ebbc58d707c3b9e5
Gabriella-Karacsonyi <gabi.karacsonyi@gmail.com>
2018-07-20 14:48:55 +0200

    feat: click counter;

--- js/app.js
+++ js/app.js
@@ -8,62 +8,50 @@ const cats = [
     src: 'images/5372754294_db6acaa1e5_z.jpg',
     name: 'Grumpy',
     count: 0,
-
   },
   {
     src: 'images/5655186625_2eedca4fc6_z.jpg',
     name: 'Tiger',
     count: 0,
-
   },
   {
     src: 'images/5675069067_c427114ce0_z.jpg',
     name:'Shylo',
     count: 0,
-
   },
   {
     src: 'images/8086455101_227ba7054d_z.jpg',
     name: 'Bob',
     count: 0,
-
-
   },
   {
     src: 'images/12367551225_3c15302538_z.jpg',
     name: 'Cutie',
     count: 0,
-
   }
 ];
 
 
 // ----------------------------------------------
-// ------------------ Init Cat  -----------------
+// --------------- Cat Selector  ----------------
 // ----------------------------------------------
 
-//figure element that holds the cat picture
+// creates a radiogroup in the DOM
 
 const catContainer = document.querySelector('.img-container');
 const catImg = document.createElement('img');
 const catName = document.createElement('figcaption');
-
-
-// ----------------------------------------------
-// --------------- Cat Selector -----------------
-// ----------------------------------------------
-
 const catSelector = document.querySelector('.cat-radiogroup');
 const counter = document.querySelector('.counter');
-// const catChooser = function(cats) {
-//   return
+
 for (let i = 0; i < cats.length; i++) {
 
     const catSelection = document.createElement('input');
     catSelection.setAttribute('type', 'radio');
     catSelection.setAttribute('id', i);
     catSelection.setAttribute('name', 'cat');
-    catSelection.setAttribute('aria-checked', 'false');
+    catSelection.setAttribute('aria-checked', false);
+    catSelection.removeAttribute('class');
     catSelection.setAttribute('value', cats[i].name);
     catSelector.appendChild(catSelection);
 
@@ -72,24 +60,49 @@ for (let i = 0; i < cats.length; i++) {
     catLabel.textContent = cats[i].name;
     catSelector.appendChild(catLabel);
 
+    // changes the radio group selection
+
     catSelection.addEventListener('click', (function(iCopy) {
+
       return function() {
            let selectedCat = cats[iCopy].src;
            let selectedName = cats[iCopy].name;
            let selectedID = cats[iCopy].id;
 
-           catSelection.setAttribute('aria-checked', 'true');
-
+           catReset();
+           catSelection.setAttribute('aria-checked', true);
+           catSelection.setAttribute('class', 'selected');
 
            catImg.setAttribute('src', selectedCat);
            catContainer.appendChild(catImg);
            selectedCat.textContent = selectedName
            catContainer.appendChild(catName);
 
+           counter.textContent = `Cat clicks: ${cats[iCopy].count}`
+
           };
       })(i));
 
 };
+
+// ----------------------------------------------
+// ------------------ Reset Cat -----------------
+// ----------------------------------------------
+//resets the the rest of the radio buttons, when a new one is selected
+
+const catChoices = document.querySelectorAll('input');
+const catReset = function() {
+  catChoices.forEach( function(catChoice){
+    catChoice.setAttribute('aria-checked', false);
+    catChoice.removeAttribute('class');
+  });
+};
+
+// ----------------------------------------------
+// ------------------ Init Cat ------------------
+// ----------------------------------------------
+
+//initializes the original selection of the first cat
 
 function initCat() {
 
@@ -97,35 +110,23 @@ function initCat() {
   catImg.setAttribute('class', 'cat-pic');
   catImg.setAttribute('src', cats[0].src);
   catContainer.appendChild(catImg);
-  document.getElementById('0').setAttribute('aria-checked', 'true');
+  document.getElementById('0').setAttribute('aria-checked', true);
+  document.getElementById('0').setAttribute('class', 'selected');
+  document.getElementById('0').setAttribute('checked', true);
 }
 
 initCat();
 
+// ----------------------------------------------
+// ----------------- Click Counter --------------
+// ----------------------------------------------
 
 catContainer.addEventListener('click', function() {
 
-  switch (catSelection.id) {
-    case 0 :
-      cats[0].count =  cats[0].count + 1;
-      counter.textContent = `Cat clicks: ${cats[0].count}`;
-      break;
-    case 1 :
-        cats[1].count =  cats[1].count + 1;
-      counter.textContent = `Cat clicks: ${cats[1].count}`;
-      break;
-    case 2 :
-        cats[2].count =  cats[2].count + 1;
-      counter.textContent = `Cat clicks: ${cats[2].count}`;
-      break;
-    case 3 :
-      cats[3].count =  cats[3].count + 1;
-      counter.textContent = `Cat clicks: ${cats[3].count}`;
-      break;
-    case 4 :
-      cats[4].count =  cats[4].count + 1;
-      counter.textContent = `Cat clicks: ${cats[4].count}`;
-      break;
-  };
+  const chosenCat = document.querySelector('.selected');
+  const id = chosenCat.getAttribute('id');
+
+  cats[id].count = cats[id].count + 1;
+  counter.textContent = `Cat clicks: ${cats[id].count}`;
 
  });
